e4b3b2b992ca5caadd583609be70aac9a9f98b02
LGehlauf <l.gehlauf@web.de>
2024-11-07 22:40:49 +0100

    241107

--- gui.py
+++ gui.py
@@ -7,6 +7,56 @@ import re
 
 # to do: auslastung der fahrzeuge, anteil an leerfahrten (zb pro Schicht), zurückgelegter Weg
 
+Lines = [   {'Start': 'a', 'End': 'c', 'Distance': 15, 'StartPoint': (100, 220), 'EndPoint': (400, 145)},
+            {'Start': 'a', 'End': 'b', 'Distance': 11, 'StartPoint': (200, 285), 'EndPoint': (400, 285)},
+            {'Start': 'a', 'End': 'd', 'Distance': 16, 'StartPoint': (100, 350), 'EndPoint': (400, 485)},
+            {'Start': 'c', 'End': 'g', 'Distance': 12, 'StartPoint': (600, 85), 'EndPoint': (800, 85)},
+            {'Start': 'c', 'End': 'b', 'Distance': 6, 'StartPoint': (500, 150), 'EndPoint': (500, 220)},
+            {'Start': 'b', 'End': 'd', 'Distance': 5, 'StartPoint': (500, 350), 'EndPoint': (500, 420)},
+            {'Start': 'd', 'End': 'e', 'Distance': 17, 'StartPoint': (600, 485), 'EndPoint': (900, 350)},
+            {'Start': 'g', 'End': 'e', 'Distance': 6, 'StartPoint': (900, 150), 'EndPoint': (900, 220)},
+            {'Start': 'e', 'End': 'f', 'Distance': 14, 'StartPoint': (1000, 285), 'EndPoint': (1200, 285)},
+]
+
+class BMG:
+    def __init__(self, ShortName, Abbreviation, Pos, LongName, Size=(50,50), Lager=False):
+        self.ShortName = ShortName
+        self.Abbreviation = Abbreviation
+        self.LongName = LongName
+        self.Pos = Pos
+        self.Size = Size
+        # left, top, width, height
+        self.main = (self.Pos[0] - 0.5 * self.Size[0], self.Pos[1] - 0.5 * self.Size[1], self.Size[0], self.Size[1])
+        self.pre = (self.main[0] - 1.2 * self.main[2], self.main[1] + 0.1 * self.main[3], 1.2 * self.main[2], 0.8 * self.main[3])
+        self.post = (self.main[0] + self.main[2], self.pre[1], self.pre[2], self.pre[3])
+        self.wrapper = (self.pre[0] - 10, self.main[1] - 50, self.pre[2] + self.main[2] + self.post[2] + 20, self.main[3] + 70)
+        self.label_text = f"{self.LongName} ({self.Abbreviation})"
+        self.Lager = Lager
+        self.StationDistance = 5
+
+    def draw(self, screen, font):
+        label = font.render(self.label_text, True, (0,0,0))
+        label_rect = label.get_rect(center=(self.Pos[0], self.main[1] - 0.5 * self.main[3]))
+        screen.blit(label, label_rect)
+        self.Rect = pygame.draw.rect(screen, (0, 0, 0), self.wrapper, width=1, border_radius=10)
+        self.Stations = (self.Rect.midleft, self.Rect.midtop, self.Rect.midright, self.Rect.midbottom)
+        for station in self.Stations:
+            pygame.draw.circle(screen, (30,30,30), station, 5)
+        if not self.Lager:
+            pygame.draw.rect(screen, (0, 0, 0), self.main, width=1, border_radius=10)
+            pygame.draw.rect(screen, (0, 0, 0), self.pre, width=1, border_radius=5)
+            pygame.draw.rect(screen, (0, 0, 0), self.post, width=1, border_radius=5)
+
+RTL = BMG('RTL', 'a', (100, 300), 'Rohteillager', Lager=True)
+SAE = BMG('SAE', 'b', (500, 300), 'Sägen')
+DRH = BMG('DRH', 'c', (500, 100), 'Drehen')
+FRA = BMG('FRA', 'd', (500, 500), 'Fräsen')
+QPR = BMG('QPR', 'e', (900, 300), 'Qualitätsprüfung')
+FTL = BMG('FTL', 'f', (1300, 300), 'Fertigteillager', Lager=True)
+LFF = BMG('LFF', 'g', (900, 100), 'Ladestation FFZ', Lager=True)
+
+BMGen = [RTL, SAE, DRH, FRA, QPR, FTL, LFF]
+
 def parse_datetime(datums_string:str):
     for format in ("%Y-%m-%d %H:%M:%S.%f", "%Y-%m-%d %H:%M:%S"):
         try:
@@ -32,54 +82,19 @@ def getTLF(cur):
         TLF.append(Dict)
     return TLF
 
-def PygameDrawStation(screen, station:dict, font):
-    main = (
-        station['Pos'][0] - 0.5 * station['Size'][0], # left
-        station['Pos'][1] - 0.5 * station['Size'][1], # top
-        station['Size'][0], # width
-        station['Size'][1] # height
-    )
-    pre = (
-        main[0] - 1.2 * main[2], # left
-        main[1] + 0.1 * main[3], # top
-        1.2 * main[2], # width
-        0.8 * main[3] # height
-    )
-    post = (
-        main[0] + main[2], # left
-        pre[1], # top
-        pre[2], # width
-        pre[3] # height
-    )
-    wrapper = (
-        pre[0] - 10,
-        main[1] - 50,
-        pre[2] + main[2] + post[2] + 20,
-        main[3] + 70,
-    )
-    label_text = f"{station['LongName']} ({station['Abbr']})"
-    label = font.render(label_text, True, (0,0,0))
-    label_rect = label.get_rect(center=(station['Pos'][0], main[1] - 0.5 * main[3]))
-    screen.blit(label, label_rect)
-
-    if station['ShortName'] not in ['RTL', 'LFF','FTL']: # lager haben keine pre und post lager
-        Rect = pygame.draw.rect(screen, (0, 0, 0), wrapper, width=1, border_radius=10)            
-        pygame.draw.rect(screen, (0, 0, 0), main, width=1, border_radius=10)
-        pygame.draw.rect(screen, (0, 0, 0), main, width=1, border_radius=10)
-        pygame.draw.rect(screen, (0, 0, 0), pre, width=1, border_radius=5)
-        pygame.draw.rect(screen, (0, 0, 0), post, width=1, border_radius=5)
-    else:
-        Rect = pygame.draw.rect(screen, (0, 0, 0), wrapper, width=1, border_radius=10)
-    return Rect
 
 def PyGameDrawClock(screen, font, clockIntern, clockExtern, fps):
-    label_text = f"intern clock (seconds): {clockIntern}"
-    label = font.render(label_text, True, (0,0,0))
-    label_rect = label.get_rect(midright=(650, 700))
-    screen.blit(label, label_rect)
+    # label_text = f"intern clock (seconds): {clockIntern}"
+    # label = font.render(label_text, True, (0,0,0))
+    # label_rect = label.get_rect(midright=(650, 700))
+    # screen.blit(label, label_rect)
     label_text = f"{clockExtern.strftime("%d. %B %Y %H:%M")}"
     label = font.render(label_text, True, (0,0,0))
-    label_rect = label.get_rect(midleft=(750, 700))
+    label_rect = label.get_rect(midleft=(650, 700))
+    screen.blit(label, label_rect)
+    label_text = f"FPS: {round(fps,1)}"
+    label = font.render(label_text, True, (0,0,0))
+    label_rect = label.get_rect(midleft=(1100, 700))
     screen.blit(label, label_rect)
 
 def CarMovement(screen, currMovement:list, lines:list, currTime, font):
@@ -107,13 +122,38 @@ def CarMovement(screen, currMovement:list, lines:list, currTime, font):
                 except:
                     pass
 
+def PyGameSampleCurrentMovements(TLF, time, cars):
+    currMovement = []
+    for line in TLF:
+        if line['Endzeitpunkt'] > time:
+            break
+        else:
+            TLF.remove(line)
+    i = 0 # counter of movements, limited to number of cars
+    for line in TLF:
+        if line['Startzeitpunkt'] <= time:
+            i += 1
+            currMovement.append(line)
+        if i >= len(cars):
+            break
+    return currMovement
+
+def PyGameDrawCars(screen, font, movement, time):
+    PyGameFindConnection(movement['Route'])
+    pass
+
+def PyGameFindConnections(Route):
+    for i in range(len(Route)):
+        if i == len(Route) - 1:
+            break
+        Route[i]
                         
 
 def initPygame(stations:list[dict], cars:set, lines:list[dict], TLF:list[dict]) -> None:
     pygame.init()
     screen = pygame.display.set_mode((1400, 800))
     clock = pygame.time.Clock()
-    framerate = 30 # 30 fps
+    framerate = 60 # fps
     SimSpeed = 3 # in seconds per minute
     font = pygame.font.SysFont(None, 20)
     ExternStartTime = TLF[0]['Startzeitpunkt']
@@ -126,57 +166,29 @@ def initPygame(stations:list[dict], cars:set, lines:list[dict], TLF:list[dict]) 
 
         screen.fill((255, 255, 255))
 
-        StationRects = []
-        for station in stations:
-            Rect = PygameDrawStation(screen, station, font)
-            StationRects.append({'ShortName': station['ShortName'], 'Rect': Rect})
+        # StationRects = []
+        # for station in stations:
+        #     Rect = PygameDrawStation(screen, station, font)
+        #     StationRects.append({'ShortName': station['ShortName'], 'Rect': Rect})
+        for bmg in BMGen:
+            bmg.draw(screen, font)
 
         currTimeIntern = pygame.time.get_ticks() / 1000
         currTimeExtern = ExternStartTime + timedelta(milliseconds=pygame.time.get_ticks()) * SimSpeed * 60
         fps = clock.get_fps()
 
-        PyGameDrawClock(screen, font ,currTimeIntern, currTimeExtern, fps)
+        PyGameDrawClock(screen, font, currTimeIntern, currTimeExtern, fps)
 
-        for line in TLF:
-            if line['Endzeitpunkt'] > currTimeExtern:
-                break
-            else:
-                TLF.remove(line)
+        currMovement = PyGameSampleCurrentMovements(TLF, currTimeExtern, cars)
 
-        currMovement = []
-        i = 0 # counter of movements, limited to number of cars
-        for line in TLF:
-            if line['Startzeitpunkt'] < currTimeExtern:
-                i += 1
-                currMovement.append(line)
-                # print(line)
-            if i >= len(cars):
-                break
-
-        CarMovement(screen, currMovement, lines, currTimeExtern, font)
+        PyGameDrawCars(screen, font, currMovement, currTimeExtern)
+        
+        # CarMovement(screen, currMovement, lines, currTimeExtern, font)
 
         pygame.display.flip()
         clock.tick(framerate)
 
-BMGen = [   {'ShortName': 'RTL','Pos': (100, 300), 'Size': (50, 50), 'Abbr': 'a', 'LongName': 'Rohteillager'},
-            {'ShortName': 'DRH','Pos': (500, 100), 'Size': (50, 50), 'Abbr': 'c', 'LongName': 'Drehen'},
-            {'ShortName': 'SAE','Pos': (500, 300), 'Size': (50, 50), 'Abbr': 'b', 'LongName': 'Sägen'},
-            {'ShortName': 'FRA','Pos': (500, 500), 'Size': (50, 50), 'Abbr': 'd', 'LongName': 'Fräsen'},
-            {'ShortName': 'LFF','Pos': (900, 100), 'Size': (50, 50), 'Abbr': 'g', 'LongName': 'Ladestation FFZ'},
-            {'ShortName': 'QPR','Pos': (900, 300), 'Size': (50, 50), 'Abbr': 'e', 'LongName': 'Qualitätsprüfung'},
-            {'ShortName': 'FTL','Pos': (1300, 300), 'Size': (50, 50), 'Abbr': 'f', 'LongName': 'Fertigteillager'}
-]
 
-Lines = [   {'Start': 'a', 'End': 'c', 'Distance': 15, 'StartPoint': (100, 220), 'EndPoint': (400, 145)},
-            {'Start': 'a', 'End': 'b', 'Distance': 11, 'StartPoint': (200, 285), 'EndPoint': (400, 285)},
-            {'Start': 'a', 'End': 'd', 'Distance': 16, 'StartPoint': (100, 350), 'EndPoint': (400, 485)},
-            {'Start': 'c', 'End': 'g', 'Distance': 12, 'StartPoint': (600, 85), 'EndPoint': (800, 85)},
-            {'Start': 'c', 'End': 'b', 'Distance': 6, 'StartPoint': (500, 150), 'EndPoint': (500, 220)},
-            {'Start': 'b', 'End': 'd', 'Distance': 5, 'StartPoint': (500, 350), 'EndPoint': (500, 420)},
-            {'Start': 'd', 'End': 'e', 'Distance': 17, 'StartPoint': (600, 485), 'EndPoint': (900, 350)},
-            {'Start': 'g', 'End': 'e', 'Distance': 6, 'StartPoint': (900, 150), 'EndPoint': (900, 220)},
-            {'Start': 'e', 'End': 'f', 'Distance': 14, 'StartPoint': (1000, 285), 'EndPoint': (1200, 285)},
-]
 
 con = sqlite3.connect('prod_data.db')
 cur = con.cursor()
